4f18e23620b178de8e05465f4cdb930a0abea1ee
pravesh20surani <pravesh20surani@gmail.com>
2018-01-23 00:13:18 -0800

    Optimized Runtime Performance
    Here we have used memoization technique without using Trie Data Structure.
    Runtime - 1487 ms.

--- LongestConcatedWord.cs
+++ LongestConcatedWord.cs
@@ -1,9 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
-
+using System.Diagnostics;
 
 namespace LongestWord
 {
@@ -15,18 +15,19 @@ namespace LongestWord
             {
                 // Here, Path is HardCoded, we can either use ServerPath or just add the file into the project
                 string path = @"D:\WordFile.txt";
-
                 LongestConcatedWord findLongest = new LongestConcatedWord();
-
+                Stopwatch stopWatch = new Stopwatch();
+                stopWatch.Start();
                 findLongest.findWord(path);
                 if (findLongest.GetConcatedWordsCount() > 0)
                 {
                     Console.WriteLine("First longest concatenated word is=" + findLongest.GetFirstLongestConcatedWord());
                     Console.WriteLine("Second longest concatenated word is=" + findLongest.GetSecondLongestConcatedWord());
-                    Console.WriteLine("number of words in it=" + findLongest.GetConcatedWordsCount());
-
+                    Console.WriteLine("Number of words in it=" + findLongest.GetConcatedWordsCount());
                 }
                 else Console.WriteLine("no concatenated word present in testfile");
+                stopWatch.Stop();
+                Console.WriteLine($"Time Taken: { stopWatch.ElapsedMilliseconds.ToString()}ms");
                 Console.ReadKey();
             }
             catch (Exception ex)
@@ -34,11 +35,12 @@ namespace LongestWord
                 Console.WriteLine(ex.ToString());
             }
         }
-
-        private Trie t = new Trie();
-        public List<String> concatedWords = new List<string>();
         private string firstLongestConcatedWord;
         private string secondLongestConcatedWord;
+        private int totalCount;
+        private Dictionary<string, int> wordDictionary = new Dictionary<string, int>();
+        private Dictionary<string, int> lookUp = new Dictionary<string, int>();
+
         public String GetFirstLongestConcatedWord()
         {
             return firstLongestConcatedWord;
@@ -49,62 +51,58 @@ namespace LongestWord
         }
         public int GetConcatedWordsCount()
         {
-            return concatedWords.Count;
+            return totalCount;
         }
-
-        public bool isRequiredWord(String word, bool fullword)
-        {
-            // Remove the word so that the word is not matched to itself to find the longest word
-            if (fullword)
-            {
-                t.RemoveWord(word);
-            }
-            // Loop over the length of the word
-            for (int i = 0; i < word.Length; i++)
-            {
-                //System.out.println(word.substring(0, i+1));
-                if (t.HasWord(word.Substring(0, i + 1)))
-                {
-                    // Check if entire word is in trie or Reminder is in trie
-                    if (i == word.Length - 1 || isRequiredWord(word.Substring(i + 1, word.Length - i - 1), false))
-                    {
-                        return true;
-                    }
-                }
-            }
-            if (fullword)
-            {
-                t.AddWord(word);
-            }
-            return false;
-        }
-
-        public void AddWordsIntoTrie(Dictionary<String, int> sortedWords)
-        {
-            foreach (string key in sortedWords.Keys)
-            {
-                t.AddWord(key);
-            }
-        }
-        public void findLongestConcatedWord(Dictionary<string, int> sortedWords)
+        public void findWord(String filePath)
         {
             try
             {
-                //traverse in sortedWords From Longest Word
-                for (int i = sortedWords.Keys.Count - 1; i >= 0; i--)
+                string line;
+                // Add each word as a key and its respective length as a value
+                StreamReader file = new StreamReader(filePath);
+                while ((line = file.ReadLine()) != null)
                 {
-                    if (isRequiredWord(sortedWords.ElementAt(i).Key, true))
+                    wordDictionary[line] = line.Length;
+                }
+                findLongestConcatedWord();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        public void findLongestConcatedWord()
+        {
+            try
+            {
+                //traverse in keys of Dictionary
+                foreach (var key in wordDictionary.Keys)
+                {
+                    if (isRequiredWord(key, true))
                     {
-                        concatedWords.Add(sortedWords.ElementAt(i).Key);
+                        totalCount++;
                     }
-                    // We can get this one also in directly output but here the purpose is for testing and making sure we are getting longest words
-                    if (concatedWords.Count == 1)
+                }
+                // Get First and Second Longest Concatenated Word
+                int maxOne = 0;
+                int maxTwo = 0;
+                firstLongestConcatedWord = "";
+                secondLongestConcatedWord = "";
+                foreach (String word in lookUp.Keys)
+                {
+                    if (maxOne < lookUp[word])
                     {
-                        firstLongestConcatedWord = concatedWords[0];
+                        maxTwo = maxOne;
+                        secondLongestConcatedWord = firstLongestConcatedWord;
+                        maxOne = lookUp[word];
+                        firstLongestConcatedWord = word;
                     }
-                    if (concatedWords.Count == 2)
+                    else if (maxTwo < lookUp[word])
                     {
-                        secondLongestConcatedWord = concatedWords[1];
+                        maxTwo = lookUp[word];
+                        secondLongestConcatedWord = word;
                     }
                 }
             }
@@ -113,39 +111,28 @@ namespace LongestWord
                 Console.WriteLine(ex.ToString());
             }
         }
-        public void findWord(String filePath)
+        public bool isRequiredWord(String word, bool fullword)
         {
-            try
+            if (!fullword && wordDictionary.ContainsKey(word))
             {
-                string line;
-                Dictionary<string, int> sortedDictionary = new Dictionary<string, int>();
-                // Add each word as a key and its respective length as a value
-                Dictionary<string, int> wordDictionary = new Dictionary<string, int>();
-
-                StreamReader file = new StreamReader(filePath);
-                while ((line = file.ReadLine()) != null)
-                {
-                    wordDictionary[line] = line.Length;
-                }
-                //insert the entries sorted by value into Dictionary.
-                var wordList = wordDictionary.ToList();
-                wordList.Sort((x, y) => x.Value.CompareTo(y.Value));
-                foreach (KeyValuePair<String, int> keyValue in wordList)
-                {
-                    var key = keyValue.Key;
-                    var value = keyValue.Value;
-                    sortedDictionary[key] = value;
-                }
-                //Adding Words to Trie Data Structure
-                AddWordsIntoTrie(sortedDictionary);
-                //Main Part of Program - Logic embeded
-                findLongestConcatedWord(sortedDictionary);
-
+                return true;
             }
-            catch (Exception e)
+            if (lookUp.ContainsKey(word))
             {
-                Console.WriteLine(e.Message);
+                return true;
             }
-        }
+            for (int i = 0; i < word.Length - 1; i++)
+            {
+                if (wordDictionary.ContainsKey(word.Substring(0, i + 1)))
+                {
+                    if (isRequiredWord(word.Substring(i + 1, word.Length - i - 1), false))
+                    {
+                        lookUp[word] = word.Length;
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }   
     }
 }
